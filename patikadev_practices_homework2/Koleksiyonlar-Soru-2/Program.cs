38ba123cab9ec322ab88d0b038ba0d59d27c442d
burak senturk <senturkburk@gmail.com>
2022-03-21 11:44:41 +0300

    2. ve 3. soru tamamlandı

--- patikadev_practices_homework2/Koleksiyonlar-Soru-2/Program.cs
+++ patikadev_practices_homework2/Koleksiyonlar-Soru-2/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 
 namespace Koleksiyonlar_Soru_2
 {
@@ -7,6 +8,70 @@ namespace Koleksiyonlar_Soru_2
         static void Main(string[] args)
         {
             // Soru - 2: Klavyeden girilen 20 adet sayının en büyük 3 tanesi ve en küçük 3 tanesi bulan, her iki grubun kendi içerisinde ortalamalarını alan ve bu ortalamaları ve ortalama toplamlarını console'a yazdıran programı yazınız.        (Array sınıfını kullanarak yazınız.)
+
+            Console.WriteLine("** 20 adet sayı giriniz: **");
+            ArrayList SayıListesi = new ArrayList();
+            int[] sayıdizisi = new int[20];
+            
+
+            for (int i = 0; i<20; i++)
+            {
+                Console.WriteLine("lütfen {0}. sayıyı giriniz: ",i+1);
+                sayıdizisi[i] = int.Parse(Console.ReadLine());
+            }
+            
+            ArrayList KucuklerListesi = new ArrayList();
+            ArrayList BuyuklerListesi = new ArrayList();
+            
+            Array.Sort(sayıdizisi);
+            for (int i = 0; i < 3; i++)
+                 KucuklerListesi.Add(sayıdizisi[i]);
+            
+            Console.WriteLine("***en küçük 3 sayı***");
+            int toplam = 0;
+            
+            foreach (var item in KucuklerListesi)
+                {
+                    Console.WriteLine(item);
+                    toplam+= Convert.ToInt32(item);
+                }
+                
+            Console.WriteLine("en küçük 3 sayının ortalaması: "+toplam/3 );
+                    
+            
+            Console.WriteLine("***en büyük 3 sayı***");
+            Array.Reverse(sayıdizisi);
+            
+            for (int i = 0; i < 3; i++)
+                BuyuklerListesi.Add(sayıdizisi[i]);
+    
+            int Toplam =0;
+            foreach (var item in BuyuklerListesi)
+                {
+                    Console.WriteLine(item);
+                    Toplam+=Convert.ToInt32(item);
+                }
+            Console.WriteLine("en büyük 3 sayının ortalaması: "+Toplam/3 );
+            
+            int OrtToplamları = Toplam/3 + toplam/3;
+            Console.WriteLine("ortalamaların toplamı: "+ OrtToplamları );
+            
+            
+
+            
+            
+
+        
+
+           
+
+
+
+
         }
     }
 }
+
+
+ //SayıListesi.Add(sayıdizisi);
+            //SayıListesi.Sort();
--- patikadev_practices_homework2/Koleksiyonlar-Soru-3/Program.cs
+++ patikadev_practices_homework2/Koleksiyonlar-Soru-3/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 
 namespace Koleksiyonlar_Soru_3
 {
@@ -7,6 +8,33 @@ namespace Koleksiyonlar_Soru_3
         static void Main(string[] args)
         {
             //Soru - 3: Klavyeden girilen cümle içerisindeki sesli harfleri bir dizi içerisinde saklayan ve dizinin elemanlarını sıralayan programı yazınız.
+
+            Console.WriteLine("**lütfen bir cümle giriniz**");
+            string cumle = Console.ReadLine().ToLower();
+            cumle.ToCharArray();
+            char[] sesliler = { 'a', 'e', 'ı', 'i', 'o', 'ö', 'u', 'ü' };
+
+            ArrayList harflistesi = new ArrayList();
+
+            foreach (var harf1 in cumle)
+            {
+                foreach (var harf2 in sesliler)
+                 {
+                  if(harf1==harf2)
+                  harflistesi.Add(harf1);
+                 }
+
+            }
+            Console.WriteLine("***cümlede bulunan sesli harfler: ***");
+
+            foreach (var item in harflistesi)
+            {
+                Console.Write(item);
+            }
+          
+
+            
+            
         }
     }
 }
