adc4e6e3b1ab8157dbd7b3744ae84eb3fca0f6b3
burak senturk <senturkburk@gmail.com>
2022-04-06 01:29:00 +0300

    Ödev tamamlandı.

--- patikadev_practices_homework2/Koleksiyonlar-Soru-1/Program.cs
+++ patikadev_practices_homework2/Koleksiyonlar-Soru-1/Program.cs
@@ -4,15 +4,43 @@ using System.Collections;
 
 namespace Koleksiyonlar_Soru_1
 {
-    class Program
+ 
+    public class Program
     {
         static void Main(string[] args)
         {
-           // Soru - 1: Klavyeden girilen 20 adet pozitif sayının asal ve asal olmayan olarak 2 ayrı listeye atın. (ArrayList sınıfını kullanarak yazınız.)
 
-           // *Negatif ve numeric olmayan girişleri engelleyin.
-           // *Her bir dizinin elemanlarını büyükten küçüğe olacak şekilde ekrana yazdırın.
-           // *Her iki dizinin eleman sayısını ve ortalamasını ekrana yazdırın
+            bool ContolMetod(int key, ArrayList liste)
+            {
+                Console.WriteLine("{0}.sayıyı giriniz :", key);
+                string n = Console.ReadLine();
+                int value;
+                if (!n.isNumeric())
+                {
+                    Console.WriteLine("Sayı giriniz");
+                    return false;
+                }
+                else
+                {
+                    value = int.Parse(n);
+                }
+                
+                if (value.isNegative())
+                {
+                    Console.WriteLine("Pozitif Sayı giriniz");
+                    return false;
+                }
+
+                liste.Add(value);
+                return true;
+
+
+            }
+            // Soru - 1: Klavyeden girilen 20 adet pozitif sayının asal ve asal olmayan olarak 2 ayrı listeye atın. (ArrayList sınıfını kullanarak yazınız.)
+
+            // *Negatif ve numeric olmayan girişleri engelleyin.
+            // *Her bir dizinin elemanlarını büyükten küçüğe olacak şekilde ekrana yazdırın.
+            // *Her iki dizinin eleman sayısını ve ortalamasını ekrana yazdırın
             Islemler islem = new Islemler();
 
             ArrayList liste = new ArrayList();
@@ -24,21 +52,18 @@ namespace Koleksiyonlar_Soru_1
             {
                 for (int i = 1; i < 21; i++)
                 {
-                    Console.WriteLine("{0}.sayıyı giriniz :", i);
-                    int n = int.Parse(Console.ReadLine());
-                    if (n < 0)
-                    {
-                        Console.WriteLine("Lütfen pozitif bir sayı giriniz");
-                        break;
-                    }
-                    liste.Add(n);
 
+                
+                    while(true){
+                        bool result=ContolMetod(i,liste);
+                        if(result) break;
+                    }
                 }
 
             }
             catch (Exception e)
             {
-                Console.WriteLine("Lüften pozitif bir sayı giriniz");
+                Console.WriteLine("Lüften pozitif bir sayı giriniz" + e.Message);
 
             }
 
@@ -112,12 +137,102 @@ namespace Koleksiyonlar_Soru_1
                 {
                     toplam += item;
                 }
-                decimal ort = toplam/arr.Count;
+                decimal ort = toplam / arr.Count;
                 Console.WriteLine("Sayıların ortalaması : " + ort);
-                  return arr;
-         }
-     }
-  }
+                return arr;
+            }
+        }
+    }
 
 }
 
+
+
+
+// class Program
+//     {
+//         static void Main(string[] args)
+//         {
+//             ArrayList numbers = new ArrayList();
+//             ArrayList asal = new ArrayList();
+//             Console.WriteLine("20 adet sayı girin.");
+
+//             try
+//             {
+
+//                 int i = 0;
+//                 while (i < 20)
+//                 {
+//                     int n = Convert.ToInt32(Console.ReadLine());
+
+//                     if (n >= 0)
+//                     {
+
+//                         if (n == 1 || n % 2 == 0 || n % 3 == 0 || n % 5 == 0 || n % 7 == 0)
+//                         {
+//                             numbers.Add(n);
+//                             if (n == 2 || n == 3 || n == 5 || n == 7)
+//                             {
+//                                 numbers.Remove(n);
+//                                 asal.Add(n);
+//                             }
+
+
+//                         }
+//                         else
+//                         {
+//                             asal.Add(n);
+//                         }
+
+//                     }
+//                     else
+//                     {
+//                         Console.WriteLine("Lütfen 0'dan büyük bir sayı girin.");
+//                         i--;
+//                     }
+
+//                     i++;
+
+//                 }
+
+//                 Console.WriteLine("******* [{0}] tane Asal Sayı vardır **********", asal.Count);
+
+//                 asal.Sort();
+//                 int asalToplam = 0;
+//                 int toplam = 0;
+
+
+
+//                 foreach (int item in asal)
+//                 {
+//                     asalToplam += item;
+//                     Console.WriteLine(item);
+
+//                 }
+
+//                 Console.WriteLine("Asal sayıların ortalaması: " + asalToplam / asal.Count);
+//                 Console.WriteLine("******* [{0}] tane Asal Olmayan Sayı vardır. **********", numbers.Count);
+
+//                 numbers.Sort();
+
+//                 foreach (int item in numbers)
+//                 {
+//                     toplam += item;
+//                     Console.WriteLine(item);
+//                 }
+//                 Console.WriteLine("Asal olmayan sayıların ortalaması: " + toplam / numbers.Count);
+//             }
+
+//             catch (Exception ex)
+//             {
+//                 Console.WriteLine("Hata: Lütfen bir sayı girin. ", ex.Message );
+
+//             }
+
+
+
+
+
+//         }
+//     }
+// }
